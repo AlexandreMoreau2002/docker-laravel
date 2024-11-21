b27f9493cb82d9fb05a55fdd79341ba7f105ab15
Alexandre <moreaualexandre2002@gmail.com>
2024-11-21 22:24:56 +0100

    feat(entrypoint): IT WORKS (hope)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Ajuster les permissions pour le dossier /app
-chmod -R 777 /app
+# Vérifier si l'environnement est Windows via la variable d'environnement
+if [[ "$WINDOWS" == "true" ]]; then
+    echo "Windows environment detected via environment variable. Adjusting permissions..."
+    chmod -R 777 /app
+else
+    echo "Skipping permissions adjustment."
+fi
 
-# Exécuter la commande passée en argument
-exec "$@"
+# Vérifier si une commande est passée en argument
+if [[ "$#" -gt 0 ]]; then
+    exec "$@"
+else
+    # Si aucune commande n'est fournie, maintenir le conteneur actif
+    echo "No command provided. Keeping the container running..."
+    tail -f /dev/null
+fi
